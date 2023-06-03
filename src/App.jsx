0b855da80224435d89d00f2c968d08e740809c5a
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-06-03 18:13:52 +0400

    coinWizards 🚀

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+
+const App = () => {
+  return (
+     <div className='app'>
+       <div className="navbar">
+
+       </div>
+       <div className="main">
+
+       </div>
+       <div className="footer">
+
+       </div>
+     </div>
+  )
+};
+
+export default App;
