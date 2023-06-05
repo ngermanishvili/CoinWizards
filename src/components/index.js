5b23ed36612268a68a0d3ca5990fdf9ccc6b1732
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-06-05 21:19:08 +0400

    update global imports component 🤡

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,6 @@ export { default as Cryptocurrencies } from './Cryptocurrencies/Cryptocurrencies
 export { default as Exchanges } from './Exchanges/Exchanges';
 export { default as Homepage } from './Homepage/Homepage';
 export { default as News } from './News/News';
+export { default as Footer } from './Footer/Footer';
 
 
