711a2c6d701edcc5185f9c8709f2c1c8c8f47ccc
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-06-05 23:56:58 +0400

    modified redux toolkin inside cryptoAPI 🤳

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -12,13 +12,13 @@ export const cryptoApi = createApi({
     baseQuery: fetchBaseQuery({ baseUrl }),
     endpoints: (builder) => ({
         getCryptos: builder.query({
-            query: () => createRequest('/coins ')
+            query: (count) => createRequest(`/coins?limit=${count}`),
         })
     })
 })
 
 export const {
-    useGetCryptosQuery, 
+    useGetCryptosQuery,
 } = cryptoApi;
 
 
