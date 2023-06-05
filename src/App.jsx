9dc5a789d782e6178a9988342cc7877b486dc744
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-06-05 15:18:39 +0400

    Configure Routes 🚀

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
+import Navbar from "./components/Navbar/Navbar";
+import "./App.css";
 
 const App = () => {
   return (
-     <div className='app'>
-       <div className="navbar">
-
-       </div>
-       <div className="main">
-
-       </div>
-       <div className="footer">
-
-       </div>
-     </div>
-  )
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main"></div>
+      <div className="footer"></div>
+    </div>
+  );
 };
 
 export default App;
