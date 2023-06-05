62e48a1c2dadd8666948113f67ab277165c4c03e
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-06-06 02:29:00 +0400

    Refactor Homepage component and add emoji 🚀

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -7,8 +7,9 @@ const { Title } = Typography;
 import Cryptocurrencies from "../Cryptocurrencies/Cryptocurrencies";
 import News from "../News/News";
 import Box from "../Animate/Box";
-import { motion } from "framer-motion";
 import Loading from "../Loading/Loading";
+import Stats from "../Stats/Stats";
+import { Stats2, Stats3 } from "../Stats/Stats";
 
 const Homepage = () => {
   const { data, isFetching } = useGetCryptosQuery(10);
@@ -19,16 +20,13 @@ const Homepage = () => {
 
   return (
     <>
-      <motion.div
-        initial={{ opacity: "0" }}
-        animate={{ opacity: "1" }}
-        exit={{ opacity: "0" }}
-      >
-        <Title level={2} className="heading">
-          Global Crypto Stats
-        </Title>
-        <Row>
-          <Col span={12}>
+      <Title level={2} className="heading">
+        GLOBAL CRYPTO STATS
+      </Title>
+      <Row>
+        <Col span={14}>
+          <Stats3 />
+          <div className="flex-row">
             <Statistic
               title="Total Cryptocurrencies"
               value={globalStats.total}
@@ -37,42 +35,47 @@ const Homepage = () => {
               title="Total Exchanges"
               value={millify(globalStats.totalExchanges)}
             />
+
             <Statistic
               title="Total Market Cap"
               value={millify(globalStats.totalMarketCap)}
             />
+          </div>
+          <div className="flex-row2">
             <Statistic
               title="Total 24h Volume"
               value={millify(globalStats.total24hVolume)}
             />
+
             <Statistic
               title="Total Markets"
               value={millify(globalStats.totalMarkets)}
             />
-          </Col>
-          <Box />
-        </Row>
-        {/* // *? Cryptocurrencies section */}
-        <div className="home-heading-container">
-          <Title level={2} className="home-title">
-            Top 10 Cryptocurrencies in the World
-          </Title>
-          <Title level={2} className="show-more">
-            <Link to="/cryptocurrencies">Show More</Link>
-          </Title>
-        </div>
-        <Cryptocurrencies simplified={true} />
-        <div className="home-heading-container">
-          {/* //*? News section News Section} */}
-          <Title level={2} className="home-title">
-            Latest Crypto News
-          </Title>
-          <Title level={2} className="show-more">
-            <Link to="/news">Show More</Link>
-          </Title>
-        </div>
-        <News simplified />
-      </motion.div>
+          </div>
+        </Col>
+        <Box />
+      </Row>
+      {/* // *? Cryptocurrencies section */}
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Top 10 Cryptocurrencies in the World
+        </Title>
+
+        <Title level={2} className="show-more">
+          <Link to="/cryptocurrencies">Show More</Link>
+        </Title>
+      </div>
+      <Cryptocurrencies simplified={true} />
+      <div className="home-heading-container">
+        {/* //*? News section News Section} */}
+        <Title level={2} className="home-title">
+          Latest Crypto News
+        </Title>
+        <Title level={2} className="show-more">
+          <Link to="/news">Show More</Link>
+        </Title>
+      </div>
+      <News simplified />
     </>
   );
 };
