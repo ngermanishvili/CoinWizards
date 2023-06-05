9fa5c57e5dbcafaf6188a45f74d30ea38be217e7
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-06-05 21:18:33 +0400

    fix: manually make key for each element 🔑

--- src/components/Cryptocurrencies/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies/Cryptocurrencies.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from "react";
+import millify from "millify";
+import { Link } from "react-router-dom";
+import { Card, Row, Col, Input } from "antd";
+import { useGetCryptosQuery } from "../../services/cryptoApi";
+
+
 
 const Cryptocurrencies = () => {
-  return (
-    <div>Cryptocurrencies</div>
-  )
-}
+  const { data: cryptoList, isFetching } = useGetCryptosQuery();
+  const cryptos = cryptoList?.data?.coins || [];
 
-export default Cryptocurrencies
+  return (
+    <>
+      <Row gutter={[32, 32]} className="crypto-card-container">
+        {cryptos.map((currency, index) => (
+          <Col
+            xs={24}
+            sm={12}
+            lg={6}
+            className="crypto-card"
+            key={`${currency.name}-${index}`}
+          >
+            <Link to={`/crypto/${currency.id}`}>
+              <Card
+                title={`${currency.rank}. ${currency.name}`}
+                extra={
+                  <img
+                    className="crypto-image"
+                    src={currency.iconUrl}
+                    alt={currency.name}
+                  />
+                }
+                hoverable
+              >
+                <p>Price: {millify(currency.price)}/</p>
+                <p>Market Cap: {millify(currency.marketCap)}/</p>
+                <p>Daily Change: {millify(currency.change)}%/</p>
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
+
+export default Cryptocurrencies;
