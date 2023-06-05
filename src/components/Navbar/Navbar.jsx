cc13c05e6b7fa30548cc08b4051cce06bd65f75b
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-06-06 02:28:06 +0400

    Refactor Navbar component and add responsive menu functionality 🚀

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import { Button, Menu, Typography, Avatar } from 'antd';
-import { Link, useLocation } from 'react-router-dom';
-import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
-
-import icon from '../images/cryptocurrency.png';
+import React, { useState, useEffect } from "react";
+import { Button, Menu, Typography, Avatar } from "antd";
+import { Link, useLocation } from "react-router-dom";
+import {
+  HomeOutlined,
+  MoneyCollectOutlined,
+  BulbOutlined,
+  FundOutlined,
+  MenuOutlined,
+} from "@ant-design/icons";
+import coinwizards from "../images/wizard3.png";
 
 const Navbar = () => {
   const [activeMenu, setActiveMenu] = useState(true);
@@ -13,11 +18,11 @@ const Navbar = () => {
   useEffect(() => {
     const handleResize = () => setScreenSize(window.innerWidth);
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
 
     handleResize();
 
-    return () => window.removeEventListener('resize', handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   useEffect(() => {
@@ -30,45 +35,49 @@ const Navbar = () => {
 
   const items = [
     {
-      key: '1',
+      key: "1",
       icon: <HomeOutlined />,
-      label: 'Home',
-      path: '/',
+      label: "HOME",
+      path: "/",
     },
     {
-      key: '2',
+      key: "2",
       icon: <FundOutlined />,
-      label: 'Cryptocurrencies',
-      path: '/cryptocurrencies',
+      label: "CRYPTOCURRENCIES",
+      path: "/cryptocurrencies",
     },
     {
-      key: '3',
+      key: "3",
       icon: <MoneyCollectOutlined />,
-      label: 'Exchanges',
-      path: '/exchanges',
+      label: "EXCHANGES",
+      path: "/exchanges",
     },
     {
-      key: '4',
+      key: "4",
       icon: <BulbOutlined />,
-      label: 'News',
-      path: '/news',
+      label: "NEWS",
+      path: "/news",
     },
   ];
 
   return (
     <div className="nav-container">
       <div className="logo-container">
-        <Avatar src={icon} size="large" />
         <Typography.Title level={2} className="logo">
-          <Link to="/">Cryptoverse</Link>
+          <Link to="/">
+            <img src={coinwizards} className="wizards" />
+          </Link>
         </Typography.Title>
-        <Button className="menu-control-container" onClick={() => setActiveMenu(!activeMenu)}>
+        <Button
+          className="menu-control-container"
+          onClick={() => setActiveMenu(!activeMenu)}
+        >
           <MenuOutlined />
         </Button>
       </div>
       {activeMenu && (
-        <Menu theme="dark" selectedKeys={[location.pathname]} >
-          {items.map(item => (
+        <Menu theme="dark" selectedKeys={[location.pathname]}>
+          {items.map((item) => (
             <Menu.Item key={item.key} icon={item.icon}>
               <Link to={item.path}>{item.label}</Link>
             </Menu.Item>
