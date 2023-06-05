88345e17dad25d8d561a956ea8759129698bfe18
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-06-05 19:16:49 +0400

    modified main jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Provider } from "react-redux";
 import App from "./App.jsx";
+import store from "./app/store.js";
+import "antd/dist/reset.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </Router>
 );
