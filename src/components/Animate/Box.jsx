8b225bf83d429514b9118b7972ca88507b81e3d7
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-06-05 23:56:07 +0400

    update box component animated 🦾

--- src/components/Animate/Box.jsx
+++ src/components/Animate/Box.jsx
@@ -1,13 +1,26 @@
 import { useRef } from "react";
 import { useAnimationFrame } from "framer-motion";
-
+import Bitcoin from "../images/btc.png";
+import Solana from "../images/sol.png";
+import Matic from "../images/matic.png";
+import Xrp from "../images/xrp.png";
 
 export default function App() {
   const ref = useRef(null);
+  const Coins = () => {
+    return (
+      <div>
+        <img src={Bitcoin} className="btc" alt="Bitcoin" />
+        <img src={Solana} className="sol" alt="Solana" />
+        <img src={Matic} className="xrp" alt="Xrp" />
+        <img src={Xrp} className="matic" alt="Matic" />
+      </div>
+    );
+  };
 
   useAnimationFrame((t) => {
-    const rotate = Math.sin(t / 12000) * 300;
-    const y = (1 + Math.sin(t / 1000)) * -30;
+    const rotate = Math.sin(t / 10000) * 200;
+    const y = (1 + Math.sin(t / 1000)) * -20;
     ref.current.style.transform = `translateY(${y}px) rotateX(${rotate}deg) rotateY(${rotate}deg)`;
   });
 
@@ -20,9 +33,8 @@ export default function App() {
         <div className="side top" />
         <div className="side bottom" />
         <div className="side back" />
+        <Coins /> <br /> <Coins /> <br /> <Coins />
       </div>
     </div>
   );
 }
-
-
