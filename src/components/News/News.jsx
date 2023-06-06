a05f3e90d9bf44c145e097dc0b3ff3ad8095f8b3
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-06-06 22:22:14 +0400

    add news

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React from "react";
+import { Select, Typography, Row, Col, Avatar, Card } from "antd";
+import { useGetCryptoNewsQuery } from "../../services/CryptoNewsApi";
+import Loading from "../Loading/Loading";
+import moment from "moment";
+const { Text, Title } = Typography;
+const { Option } = Select;
+const demoImage =
+  "https://coinclarity.com/wp-content/uploads/2020/04/coinclarity-placeholder.png";
 
-const News = () => {
+const News = ({ simplified }) => {
+  const { data: cryptoNews } = useGetCryptoNewsQuery({
+    newsCategory: "Cryptocurrency",
+    count: simplified ? 6 : 12,
+  });
+
+  if (!cryptoNews?.value) return <Loading />;
+
   return (
-    <div>News</div>
-  )
-}
+    <Row gutter={[24, 24]}>
+      {cryptoNews.value.map((news, i) => (
+        <Col xs={24} sm={12} lg={8} key={i}>
+          <Card hoverable className="news-card">
+            <a href={news.url} target="_blank" rel="noreferrer">
+              <div className="news-image-container">
+                <Title className="news-title" level={4}>
+                  {news.name}
+                </Title>
+                <img className="   "
+                  src={news?.image?.thumbnail?.contentUrl || demoImage}
+                  alt=""
+                />
+              </div>
+              <p>
+                {news.description.length > 100
+                  ? `${news.description.substring(0, 100)}...`
+                  : news.description}
+              </p>
+              <div className="provider-container">
+                <div>
+                  <Avatar
+                    src={
+                      news.provider[0]?.image?.thumbnail?.contentUrl ||
+                      demoImage
+                    }
+                    alt=""
+                  />
+                  <Text className="provider-name">
+                    {news.provider[0]?.name}
+                  </Text>
+                </div>
+                <Text>
+                  {moment(news.datePublished).startOf("ss").fromNow()}
+                </Text>
+              </div>
+            </a>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  );
+};
 
-export default News
+export default News;
