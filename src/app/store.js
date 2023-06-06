07a80b205ea2975c0ea79ec1baf573a4dcfef371
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-06-06 22:22:41 +0400

    update store redux functionally

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { cryptoApi } from '../services/cryptoApi';
+import { cryptoNewsApi } from '../services/CryptoNewsApi';
 
 const cryptoApiMiddleware = cryptoApi.middleware;
+const cryptoNewsApiMiddleware = cryptoNewsApi.middleware;
+
 
 export default configureStore({
     reducer: {
         [cryptoApi.reducerPath]: cryptoApi.reducer,
+        [cryptoNewsApi.reducerPath]: cryptoNewsApi.reducer,
+
     },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(cryptoApiMiddleware),
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(cryptoApiMiddleware, cryptoNewsApiMiddleware),
 });
