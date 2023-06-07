a94c64201f4feabd09548a4e6ea2aae13a312a0c
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-06-07 23:57:24 +0400

    modified home component

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import millify from "millify";
-import { Typography, Row, Col, Statistic } from "antd";
+import { Typography, Row, Col, Statistic, Card } from "antd";
 import { Link } from "react-router-dom";
 import { useGetCryptosQuery } from "../../services/cryptoApi";
 const { Title } = Typography;
@@ -12,6 +12,7 @@ import Stats from "../Stats/Stats";
 import { Stats2, Stats3 } from "../Stats/Stats";
 import Owl from "../Carousel/Carousel";
 import ProgressBar from "../ProgressBar/ProgressBar";
+import styled from "styled-components";
 
 const Homepage = () => {
   const { data, isFetching } = useGetCryptosQuery(10);
@@ -22,69 +23,76 @@ const Homepage = () => {
 
   return (
     <>
-      <Title level={2} className="heading">
-        GLOBAL CRYPTO STATS
-      </Title>
-      <Row>
-        <Col span={14}>
-          <Stats3 />
-          <div className="flex-row">
-            <Statistic
-              title="Total Cryptocurrencies"
-              value={globalStats.total}
-            />
-            <Statistic
-              title="Total Exchanges"
-              value={millify(globalStats.totalExchanges)}
-            />
+      <Wrapper>
+    
+          <Title level={2} className="heading">
+            GLOBAL CRYPTO STATS
+          </Title>
+          <Row>
+            <Col span={14}>
+              <Stats3 />
+              <div className="flex-row">
+                <Statistic
+                  title="Total Cryptocurrencies"
+                  value={globalStats.total}
+                />
+                <Statistic
+                  title="Total Exchanges"
+                  value={millify(globalStats.totalExchanges)}
+                />
 
-            <Statistic
-              title="Total Market Cap"
-              value={millify(globalStats.totalMarketCap)}
-            />
-          </div>
-          <div className="flex-row2">
-            <Statistic
-              title="Total 24h Volume"
-              value={millify(globalStats.total24hVolume)}
-            />
+                <Statistic
+                  title="Total Market Cap"
+                  value={millify(globalStats.totalMarketCap)}
+                />
+              </div>
+              <div className="flex-row2">
+                <Statistic
+                  title="Total 24h Volume"
+                  value={millify(globalStats.total24hVolume)}
+                />
 
-            <Statistic
-              title="Total Markets"
-              value={millify(globalStats.totalMarkets)}
-            />
-          </div>
-        </Col>
-        <Box />
-      </Row>
-      {/* // *? Cryptocurrencies section */}
-      <Owl />
-      <div className="home-heading-container">
-        <Title level={2} className="home-title">
-          Top 10 Cryptocurrencies in the World
-        </Title>
+                <Statistic
+                  title="Total Markets"
+                  value={millify(globalStats.totalMarkets)}
+                />
+              </div>
+            </Col>
+            <Box />
+          </Row>
+  
 
-        <Title level={2} className="show-more">
-          <Link to="/cryptocurrencies">Show More</Link>
-        </Title>
-      </div>
+        {/* // *? Cryptocurrencies section */}
+        <Owl />
+        <div className="home-heading-container">
+          <Title level={2} className="home-title">
+            Top 10 Cryptocurrencies in the World
+          </Title>
 
-      <Cryptocurrencies simplified={true} />
+          <Title level={2} className="show-more">
+            <Link to="/cryptocurrencies">Show More</Link>
+          </Title>
+        </div>
 
-      {/* <ProgressBar /> */}
-      <div className="home-heading-container">
-        {/* //*? News section News Section} */}
-        {/* <Title level={2} className="home-title">
+        <Cryptocurrencies simplified={true} />
+
+        {/* <ProgressBar /> */}
+        <div className="home-heading-container">
+          {/* //*? News section News Section} */}
+          {/* <Title level={2} className="home-title">
           Latest Crypto News
         </Title>
 
         <Title level={2} className="show-more">
           <Link to="/news">Show More .. </Link>
         </Title> */}
-      </div>
-      {/* <News simplified /> */}
+        </div>
+      </Wrapper>
     </>
   );
 };
 
+const Wrapper = styled.div`
+
+`;
 export default Homepage;
