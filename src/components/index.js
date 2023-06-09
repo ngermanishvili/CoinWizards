6a30dcaa800f9ddfadf7f78aae5670eba24695e1
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-06-09 04:45:27 +0400

    Export individual components from their respective modules

--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,7 @@ export { default as Cryptocurrencies } from './Cryptocurrencies/Cryptocurrencies
 export { default as Exchanges } from './Exchanges/Exchanges';
 export { default as Homepage } from './Homepage/Homepage';
 export { default as News } from './News/News';
+export { default as About } from './About/About';
 export { default as Footer } from './Footer/Footer';
 
 
