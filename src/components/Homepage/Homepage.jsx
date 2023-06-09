48060c0e78a162dcf8ef217006f57327d49d6360
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-06-09 04:47:35 +0400

    Update Homepage component with improved UI, statistics, and cryptocurrency cards

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -13,6 +13,10 @@ import { Stats2, Stats3 } from "../Stats/Stats";
 import Owl from "../Carousel/Carousel";
 import ProgressBar from "../ProgressBar/ProgressBar";
 import styled from "styled-components";
+import Cards from "../Cards/Cards";
+
+// import Banner from "../Banner/Banner";
+// import { Banner01DataSource } from "../../data/data.source";
 
 const Homepage = () => {
   const { data, isFetching } = useGetCryptosQuery(10);
@@ -23,56 +27,93 @@ const Homepage = () => {
 
   return (
     <>
+      {/* <Wrapper>
+        <Title level={2} className="heading">
+          GLOBAL CRYPTO STATS
+        </Title>
+        <Row>
+          <Col span={14}>
+            <Stats3 />
+            <div className="flex-row">
+              <Statistic
+                title="Total Cryptocurrencies"
+                value={globalStats.total}
+              />
+              <Statistic
+                title="Total Exchanges"
+                value={millify(globalStats.totalExchanges)}
+              />
+
+              <Statistic
+                title="Total Market Cap"
+                value={millify(globalStats.totalMarketCap)}
+              />
+            </div>
+            <div className="flex-row2">
+              <Statistic
+                title="Total 24h Volume"
+                value={millify(globalStats.total24hVolume)}
+              />
+
+              <Statistic
+                title="Total Markets"
+                value={millify(globalStats.totalMarkets)}
+              />
+            </div>
+          </Col>
+          <Box />
+        </Row> */}
+
+      {/* // *? Cryptocurrencies section */}
+
+      <Cards />
+
+      <Owl />
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Top 10 Cryptocurrencies in the World
+        </Title>
+
+        <Title level={2} className="show-more">
+          <Link to="/cryptocurrencies">Show More</Link>
+        </Title>
+      </div>
       <Wrapper>
-    
-          <Title level={2} className="heading">
-            GLOBAL CRYPTO STATS
-          </Title>
-          <Row>
-            <Col span={14}>
-              <Stats3 />
-              <div className="flex-row">
-                <Statistic
-                  title="Total Cryptocurrencies"
-                  value={globalStats.total}
-                />
-                <Statistic
-                  title="Total Exchanges"
-                  value={millify(globalStats.totalExchanges)}
-                />
+        <Title level={2} className="heading">
+          GLOBAL CRYPTO STATS
+        </Title>
+        <Row>
+          <Col span={14}>
+            <Stats3 />
+            <div className="flex-row">
+              <Statistic
+                title="Total Cryptocurrencies"
+                value={globalStats.total}
+              />
+              <Statistic
+                title="Total Exchanges"
+                value={millify(globalStats.totalExchanges)}
+              />
 
-                <Statistic
-                  title="Total Market Cap"
-                  value={millify(globalStats.totalMarketCap)}
-                />
-              </div>
-              <div className="flex-row2">
-                <Statistic
-                  title="Total 24h Volume"
-                  value={millify(globalStats.total24hVolume)}
-                />
+              <Statistic
+                title="Total Market Cap"
+                value={millify(globalStats.totalMarketCap)}
+              />
+            </div>
+            <div className="flex-row2">
+              <Statistic
+                title="Total 24h Volume"
+                value={millify(globalStats.total24hVolume)}
+              />
 
-                <Statistic
-                  title="Total Markets"
-                  value={millify(globalStats.totalMarkets)}
-                />
-              </div>
-            </Col>
-            <Box />
-          </Row>
-  
-
-        {/* // *? Cryptocurrencies section */}
-        <Owl />
-        <div className="home-heading-container">
-          <Title level={2} className="home-title">
-            Top 10 Cryptocurrencies in the World
-          </Title>
-
-          <Title level={2} className="show-more">
-            <Link to="/cryptocurrencies">Show More</Link>
-          </Title>
-        </div>
+              <Statistic
+                title="Total Markets"
+                value={millify(globalStats.totalMarkets)}
+              />
+            </div>
+          </Col>
+          <Box />
+        </Row>
 
         <Cryptocurrencies simplified={true} />
 
@@ -92,7 +133,5 @@ const Homepage = () => {
   );
 };
 
-const Wrapper = styled.div`
-
-`;
+const Wrapper = styled.div``;
 export default Homepage;
