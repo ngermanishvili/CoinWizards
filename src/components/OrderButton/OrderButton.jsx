700f0d7da258e3011683d6567a0d2cbf8bfc6c7c
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-06-11 02:08:31 +0400

    order btn update

--- src/components/OrderButton/OrderButton.jsx
+++ src/components/OrderButton/OrderButton.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { Button, Space } from "antd";
 
-const App = () => (
-  <Space direction="vertical" style={{ width: "100%" }}>
-    <Button type="primary" block>
-      Primary
-    </Button>
-    <Button block>Default</Button>
-  </Space>
-);
+const OrderButton = () => {
+  const handleButtonClick = () => {
+    window.location.href = "https://t.me/your_telegram_page";
+  };
 
-export default App;
+  return (
+    <Space direction="vertical" style={{ width: "100%" }}>
+      <Button type="primary" block onClick={handleButtonClick}>
+        აქტივაცია
+      </Button>
+      <Button block>გაიარე კონსულტაცია</Button>
+    </Space>
+  );
+};
+
+export default OrderButton;
