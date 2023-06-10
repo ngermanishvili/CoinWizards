837f75a39efcfd9be67d398f873453c8f4c8b302
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-06-10 18:57:25 +0400

    update cards and add dummy data for card info

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,9 +1,4 @@
 import React from "react";
-import {
-  EditOutlined,
-  EllipsisOutlined,
-  SettingOutlined,
-} from "@ant-design/icons";
 import { Avatar, Card, Row, Col } from "antd";
 import TimelineText from "../Timeline/Timeline";
 import styled from "styled-components";
@@ -11,135 +6,68 @@ import OrderButton from "../OrderButton/OrderButton";
 
 const { Meta } = Card;
 
+const cardsData = [
+  {
+    id: 1,
+    title: "Basic 28$",
+    avatarSrc: "https://xsgames.co/randomusers/avatar.php?g=pixel",
+    imageSrc:
+      "https://support.trailingcrypto.com/wp-content/uploads/2021/08/Crypto-Signals-1-1024x576.jpg",
+  },
+  {
+    id: 1,
+    title: "Basic 28$",
+    avatarSrc: "https://xsgames.co/randomusers/avatar.php?g=pixel",
+    imageSrc:
+      "https://support.trailingcrypto.com/wp-content/uploads/2021/08/Crypto-Signals-1-1024x576.jpg",
+  },
+  {
+    id: 1,
+    title: "Basic 28$",
+    avatarSrc: "https://xsgames.co/randomusers/avatar.php?g=pixel",
+    imageSrc:
+      "https://support.trailingcrypto.com/wp-content/uploads/2021/08/Crypto-Signals-1-1024x576.jpg",
+  },
+  {
+    id: 1,
+    title: "Basic 28$",
+    avatarSrc: "https://xsgames.co/randomusers/avatar.php?g=pixel",
+    imageSrc:
+      "https://support.trailingcrypto.com/wp-content/uploads/2021/08/Crypto-Signals-1-1024x576.jpg",
+  },
+  {
+    id: 1,
+    title: "Basic 28$",
+    avatarSrc: "https://xsgames.co/randomusers/avatar.php?g=pixel",
+    imageSrc:
+      "https://support.trailingcrypto.com/wp-content/uploads/2021/08/Crypto-Signals-1-1024x576.jpg",
+  },
+  {
+    id: 1,
+    title: "Basic 28$",
+    avatarSrc: "https://xsgames.co/randomusers/avatar.php?g=pixel",
+    imageSrc:
+      "https://support.trailingcrypto.com/wp-content/uploads/2021/08/Crypto-Signals-1-1024x576.jpg",
+  },
+  // Add more card data objects here
+];
+
 const Cards = () => (
   <Wrapper>
-    <Row gutter={[24, 24]}>
-      <Col xs={24} sm={12} md={8}>
-        <Card
-          style={{ width: "80%" }}
-          cover={
-            <img
-              alt="example"
-              src="https://support.trailingcrypto.com/wp-content/uploads/2021/08/Crypto-Signals-1-1024x576.jpg"
-            />
-          }
-        >
-          <Meta
-            avatar={
-              <Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel" />
-            }
-            title="Basic 28$"
-          />
-          <br />
-          <TimelineText />
-          <OrderButton />
-        </Card>
-      </Col>
-      <Col xs={24} sm={12} md={8}>
-        <Card
-          style={{ width: "80%" }}
-          cover={
-            <img
-              alt="example"
-              src="https://support.trailingcrypto.com/wp-content/uploads/2021/08/Crypto-Signals-1-1024x576.jpg"
-            />
-          }
-        >
-          <Meta
-            avatar={
-              <Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel" />
-            }
-            title="Premium 40$"
-          />
-          <br />
-          <TimelineText />
-          <OrderButton />
-        </Card>
-      </Col>
-      <Col xs={24} sm={12} md={8}>
-        <Card
-          style={{ width: "80%" }}
-          cover={
-            <img
-              alt="example"
-              src="https://support.trailingcrypto.com/wp-content/uploads/2021/08/Crypto-Signals-1-1024x576.jpg"
-            />
-          }
-        >
-          <Meta
-            avatar={
-              <Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel" />
-            }
-            title="Ultimate 56$"
-          />
-          <br />
-          <TimelineText />
-          <OrderButton />
-        </Card>
-      </Col>
-      <Col xs={24} sm={12} md={8}>
-        <Card
-          style={{ width: "80%" }}
-          cover={
-            <img
-              alt="example"
-              src="https://support.trailingcrypto.com/wp-content/uploads/2021/08/Crypto-Signals-1-1024x576.jpg"
-            />
-          }
-        >
-          <Meta
-            avatar={
-              <Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel" />
-            }
-            title="Ultimate 56$"
-          />
-          <br />
-          <TimelineText />
-          <OrderButton />
-        </Card>
-      </Col>
-      <Col xs={24} sm={12} md={8}>
-        <Card
-          style={{ width: "80%" }}
-          cover={
-            <img
-              alt="example"
-              src="https://support.trailingcrypto.com/wp-content/uploads/2021/08/Crypto-Signals-1-1024x576.jpg"
-            />
-          }
-        >
-          <Meta
-            avatar={
-              <Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel" />
-            }
-            title="Ultimate 56$"
-          />
-          <br />
-          <TimelineText />
-          <OrderButton />
-        </Card>
-      </Col>
-      <Col xs={24} sm={12} md={8}>
-        <Card
-          style={{ width: "80%" }}
-          cover={
-            <img
-              alt="example"
-              src="https://support.trailingcrypto.com/wp-content/uploads/2021/08/Crypto-Signals-1-1024x576.jpg"
-            />
-          }
-        >
-          <Meta
-            avatar={
-              <Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel" />
-            }
-            title="Ultimate 56$"
-          />
-          <br />
-          <TimelineText />
-          <OrderButton />
-        </Card>
-      </Col>
+    <Row gutter={[32, 32]}>
+      {cardsData.map((card) => (
+        <Col xs={24} sm={12} md={8} key={card.id}>
+          <Card
+            style={{ maxWidth: 350, width: "100%" }}
+            cover={<img alt="example" src={card.imageSrc} />}
+          >
+            <Meta avatar={<Avatar src={card.avatarSrc} />} title={card.title} />
+            <br />
+            <TimelineText />
+            <OrderButton />
+          </Card>
+        </Col>
+      ))}
     </Row>
   </Wrapper>
 );
