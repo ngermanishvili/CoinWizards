576ae88250b6e7e15692aec39403db06da960e80
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-06-10 18:33:35 +0400

    add payment btn to cards

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -4,101 +4,149 @@ import {
   EllipsisOutlined,
   SettingOutlined,
 } from "@ant-design/icons";
-import { Avatar, Card } from "antd";
+import { Avatar, Card, Row, Col } from "antd";
 import TimelineText from "../Timeline/Timeline";
 import styled from "styled-components";
+import OrderButton from "../OrderButton/OrderButton";
 
 const { Meta } = Card;
 
 const Cards = () => (
   <Wrapper>
-    <CardContainer>
-      <Card
-        style={{ width: 350 }}
-        cover={
-          <img
-            alt="example"
-            src="https://support.trailingcrypto.com/wp-content/uploads/2021/08/Crypto-Signals-1-1024x576.jpg"
-          />
-        }
-        actions={[
-          <SettingOutlined key="setting" />,
-          <EditOutlined key="edit" />,
-          <EllipsisOutlined key="ellipsis" />,
-        ]}
-      >
-        <Meta
-          avatar={
-            <Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel" />
+    <Row gutter={[24, 24]}>
+      <Col xs={24} sm={12} md={8}>
+        <Card
+          style={{ width: "80%" }}
+          cover={
+            <img
+              alt="example"
+              src="https://support.trailingcrypto.com/wp-content/uploads/2021/08/Crypto-Signals-1-1024x576.jpg"
+            />
           }
-          title="Basic 28$"
-        />
-        <br />
-        <TimelineText />
-      </Card>
-    </CardContainer>
-    <CardContainer>
-      <Card
-        style={{ width: 350 }}
-        cover={
-          <img
-            alt="example"
-            src="https://support.trailingcrypto.com/wp-content/uploads/2021/08/Crypto-Signals-1-1024x576.jpg"
+        >
+          <Meta
+            avatar={
+              <Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel" />
+            }
+            title="Basic 28$"
           />
-        }
-        actions={[
-          <SettingOutlined key="setting" />,
-          <EditOutlined key="edit" />,
-          <EllipsisOutlined key="ellipsis" />,
-        ]}
-      >
-        <Meta
-          avatar={
-            <Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel" />
+          <br />
+          <TimelineText />
+          <OrderButton />
+        </Card>
+      </Col>
+      <Col xs={24} sm={12} md={8}>
+        <Card
+          style={{ width: "80%" }}
+          cover={
+            <img
+              alt="example"
+              src="https://support.trailingcrypto.com/wp-content/uploads/2021/08/Crypto-Signals-1-1024x576.jpg"
+            />
           }
-          title="Premium 40$"
-        />
-        <br />
-        <TimelineText />
-      </Card>
-    </CardContainer>
-    <CardContainer>
-      <Card
-        style={{ width: 350 }}
-        cover={
-          <img
-            alt="example"
-            src="https://support.trailingcrypto.com/wp-content/uploads/2021/08/Crypto-Signals-1-1024x576.jpg"
+        >
+          <Meta
+            avatar={
+              <Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel" />
+            }
+            title="Premium 40$"
           />
-        }
-        actions={[
-          <SettingOutlined key="setting" />,
-          <EditOutlined key="edit" />,
-          <EllipsisOutlined key="ellipsis" />,
-        ]}
-      >
-        <Meta
-          avatar={
-            <Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel" />
+          <br />
+          <TimelineText />
+          <OrderButton />
+        </Card>
+      </Col>
+      <Col xs={24} sm={12} md={8}>
+        <Card
+          style={{ width: "80%" }}
+          cover={
+            <img
+              alt="example"
+              src="https://support.trailingcrypto.com/wp-content/uploads/2021/08/Crypto-Signals-1-1024x576.jpg"
+            />
           }
-          title="Ultimate 56$"
-        />
-        <br />
-        <TimelineText />
-      </Card>
-    </CardContainer>
+        >
+          <Meta
+            avatar={
+              <Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel" />
+            }
+            title="Ultimate 56$"
+          />
+          <br />
+          <TimelineText />
+          <OrderButton />
+        </Card>
+      </Col>
+      <Col xs={24} sm={12} md={8}>
+        <Card
+          style={{ width: "80%" }}
+          cover={
+            <img
+              alt="example"
+              src="https://support.trailingcrypto.com/wp-content/uploads/2021/08/Crypto-Signals-1-1024x576.jpg"
+            />
+          }
+        >
+          <Meta
+            avatar={
+              <Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel" />
+            }
+            title="Ultimate 56$"
+          />
+          <br />
+          <TimelineText />
+          <OrderButton />
+        </Card>
+      </Col>
+      <Col xs={24} sm={12} md={8}>
+        <Card
+          style={{ width: "80%" }}
+          cover={
+            <img
+              alt="example"
+              src="https://support.trailingcrypto.com/wp-content/uploads/2021/08/Crypto-Signals-1-1024x576.jpg"
+            />
+          }
+        >
+          <Meta
+            avatar={
+              <Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel" />
+            }
+            title="Ultimate 56$"
+          />
+          <br />
+          <TimelineText />
+          <OrderButton />
+        </Card>
+      </Col>
+      <Col xs={24} sm={12} md={8}>
+        <Card
+          style={{ width: "80%" }}
+          cover={
+            <img
+              alt="example"
+              src="https://support.trailingcrypto.com/wp-content/uploads/2021/08/Crypto-Signals-1-1024x576.jpg"
+            />
+          }
+        >
+          <Meta
+            avatar={
+              <Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel" />
+            }
+            title="Ultimate 56$"
+          />
+          <br />
+          <TimelineText />
+          <OrderButton />
+        </Card>
+      </Col>
+    </Row>
   </Wrapper>
 );
 
 const Wrapper = styled.div`
-margin-top: 600px;   
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const CardContainer = styled.div`
-  flex-basis: 30%;
+  margin-bottom: 300px;
+  padding: 16px;
 `;
 
 export default Cards;
