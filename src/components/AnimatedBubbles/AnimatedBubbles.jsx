1ccee2a10e92428a93eea9f8bd49273e9d3e9075
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-06-13 01:42:12 +0400

    update bbls

--- src/components/AnimatedBubbles/AnimatedBubbles.jsx
+++ src/components/AnimatedBubbles/AnimatedBubbles.jsx
@@ -117,43 +117,99 @@
 // `;
 // export default Bubbles;
 
-import React, { useState, useEffect, useRef } from 'react';
-import * as d3 from 'd3';
+import React, { useState, useEffect, useRef } from "react";
+import * as d3 from "d3";
 
 const AnimatedBubbles = () => {
   const [data, setData] = useState([]);
   const svgRef = useRef(null);
+  const radiusScale = d3.scaleLinear().range([30, 70]);
 
   const createBubbleChart = () => {
     const svg = d3.select(svgRef.current);
-    const margin = { top: 20, right: 20, bottom: 20, left: 20 };
-    const width = svg.attr('width') - margin.left - margin.right;
-    const height = svg.attr('height') - margin.top - margin.bottom;
-    const radiusScale = d3.scaleLinear()
-      .range([2, 10])
-      .domain([0, d3.max(data, d => d.size)]);
-    const colorScale = d3.scaleOrdinal()
-      .range(['red', 'green', 'blue'])
-      .domain(data.map(d => d.color));
+    const margin = { top: 50, right: 20, bottom: 20, left: 20 };
+    const width = window.innerWidth - margin.left - margin.right;
+    const height = window.innerHeight - margin.top - margin.bottom;
 
-    svg.append('g')
-      .attr('class', 'bubbles')
-      .selectAll('circle')
+    radiusScale.domain([0, d3.max(data, (d) => d.size)]);
+
+    const simulation = d3
+      .forceSimulation(data)
+      //   .force("x", d3.forceX(width / 3).strength(0.050))
+      //   .force("y", d3.forceY(height / 2).strength(0.06))
+      .force(
+        "collide",
+        d3
+          .forceCollide(2)
+          .radius((d) => radiusScale(d.size) + 2)
+          .strength(2)
+      )
+      .force("charge", d3.forceManyBody().strength(1));
+
+    const colorScale = d3
+      .scaleOrdinal()
+      .range(["#f44336", "#4caf50"])
+
+      .domain(data.map((d) => d.color));
+
+    svg.select(".bubbles").remove();
+
+    const bubblesGroup = svg.append("g").attr("class", "bubbles");
+
+    const circles = bubblesGroup
+      .selectAll("circle")
       .data(data)
       .enter()
-      .append('circle')
-      .attr('cx', d => d.x)
-      .attr('cy', d => d.y)
-      .attr('r', d => radiusScale(d.size))
-      .attr('fill', d => colorScale(d.color));
+      .append("circle")
+      .attr("cx", (d) => d.x)
+      .attr("cy", (d) => d.y)
+      .attr("r", (d) => radiusScale(d.size))
+      .attr("fill", (d) => colorScale(d.color))
+      .style("stroke", "blue") // Set the border color to blue
+      .style("stroke-width", "1px") // Set the border width to 1 pixel
+      .style("box-shadow", "0px 0px 10px black, 0px 0px 10px black inset") // Add shadows to both border and inside
+      .style("cursor", "pointer")
+      .style("fill-opacity", 0.75);
+
+    const moveCircles = () => {
+      circles
+        .attr("cx", (d) => {
+          d.x += (Math.random() - 0.05) * 0.08; // Adjust the movement speed here
+          return d.x;
+        })
+        .attr("cy", (d) => {
+          d.y += (Math.random() - 0.03) * 0.06; // Adjust the movement speed here
+          return d.y;
+        });
+    };
+
+    d3.interval(moveCircles, 5); // Adjust the interval duration here
   };
 
   useEffect(() => {
-    setData([
-      { color: 'red', size: 20, x: 100, y: 100 },
-      { color: 'green', size: 20, x: 200, y: 200 },
-      { color: 'blue', size: 20, x: 300, y: 300 }
-    ]);
+    const generateRandomBubbles = () => {
+      const newBubbles = Array.from({ length: 60 }, () => ({
+        color: getRandomColor(),
+        size: getRandomSize(),
+        x: Math.random() * window.innerWidth,
+        y: Math.random() * window.innerHeight,
+      }));
+      setData(newBubbles);
+    };
+
+    const getRandomColor = () => {
+      const colors = ["red", "green"];
+      return colors[Math.floor(Math.random() * colors.length)];
+    };
+
+    const getRandomSize = () => Math.floor(Math.random() * (50 - 10 + 1) + 10);
+
+    generateRandomBubbles();
+    window.addEventListener("resize", generateRandomBubbles);
+
+    return () => {
+      window.removeEventListener("resize", generateRandomBubbles);
+    };
   }, []);
 
   useEffect(() => {
@@ -163,12 +219,14 @@ const AnimatedBubbles = () => {
   }, [data]);
 
   return (
-    <div>
-      <svg ref={svgRef} width="500" height="500" viewBox="0 0 500 500"></svg>
-    </div>
+    <svg
+      className="bubbles-svg"
+      ref={svgRef}
+      width={window.innerWidth}
+      height={window.innerHeight}
+      viewBox={`0 0 ${window.innerWidth} ${window.innerHeight}`}
+    ></svg>
   );
 };
 
 export default AnimatedBubbles;
-
-
