fb84cbd0a944ff01b18b4df6fec2ca58a52f478a
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-06-15 21:54:40 +0400

    about moidif

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Bubbles from "../AnimatedBubbles/AnimatedBubbles"; // Import the Bubbles component
+import styled from "styled-components";
 
 const About = () => {
-  return <div>About</div>;
+  // About page content
+  return (
+    <div className="bubblediv">
+      <Bubbles />
+    </div>
+  );
 };
 
 export default About;
