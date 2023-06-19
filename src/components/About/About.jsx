1cc56b07928ddf75a3bd7a47627312c8238ce969
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-06-19 21:37:14 +0400

    about info soon modified to bubble route

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,14 +1,38 @@
 import React from "react";
 import Bubbles from "../AnimatedBubbles/AnimatedBubbles"; // Import the Bubbles component
 import styled from "styled-components";
+import { Card, Col, Row, Statistic } from "antd";
 
 const About = () => {
   // About page content
   return (
-    <div className="bubblediv">
-      <Bubbles />
-    </div>
+    <>
+      <Wrapper>
+        <div className="bubblediv">
+          <Bubbles />
+        </div>
+      </Wrapper>
+    </>
   );
 };
+
+const Wrapper = styled.div`
+  .bubblediv {
+    margin-top: 31px;
+    width: 100%;
+    height: 800px;
+    position: relative;
+    background: #261e35;
+  }
+
+  @media screen and (max-width: 800px) {
+    .bubblediv {
+      width: 100%;
+      height: 100vh;
+      bottom: 0;
+      position: absolute;
+    }
+  }
+`;
 
 export default About;
