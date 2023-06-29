dda35fcaee1c951383e4f16db0792a6d2a8754d3
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-06-29 23:16:05 +0400

    update cryptos

--- src/components/Cryptocurrencies/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies/Cryptocurrencies.jsx
@@ -5,6 +5,7 @@ import { Card, Row, Col, Input } from "antd";
 import { useGetCryptosQuery } from "../../services/cryptoApi";
 import Loading from "../Loading/Loading";
 import Owl from "../Carousel/Carousel";
+import GifCard from "../GifCard/GifCard";
 
 const Cryptocurrencies = ({ simplified }) => {
   const count = simplified ? 10 : 100;
@@ -31,37 +32,39 @@ const Cryptocurrencies = ({ simplified }) => {
       )}
       {/* <Owl /> */}
       <Row gutter={[32, 32]} className="crypto-card-container">
-        {filteredCryptos.map(
-          (currency) => {
-            return (
-              <Col
-                xs={24}
-                sm={12}
-                lg={6}
-                className="crypto-card"
-                key={`${currency.name}-${currency.uuid}`}
-              >
-                <Link to={`/crypto/${currency.uuid}`}>
-                  <Card
-                    title={`${currency.rank}. ${currency.name}`}
-                    extra={
-                      <img
-                        className="crypto-image"
-                        src={currency.iconUrl}
-                        alt={currency.name}
-                      />
-                    }
-                    hoverable
-                  >
-                    <p>Price: {millify(currency.price)}/</p>
-                    <p>Market Cap: {millify(currency.marketCap)}/</p>
-                    <p>Daily Change: {millify(currency.change)}%/</p>
-                  </Card>
-                </Link>
-              </Col>
-            );
-          }
-        )}
+        {filteredCryptos.map((currency) => {
+          return (
+            <Col
+              xs={24}
+              sm={12}
+              lg={6}
+              className="crypto-card"
+              key={`${currency.name}-${currency.uuid}`}
+            >
+              <Link to={`/crypto/${currency.uuid}`}>
+           
+
+                <Card
+                  title={`${currency.rank}. ${currency.name}`}
+                  extra={
+                    <img
+                      className="crypto-image"
+                      src={currency.iconUrl}
+                      alt={currency.name}
+                    />
+                  }
+                  hoverable
+                >
+                  <p>Price: {millify(currency.price)}/</p>
+                  <p>Market Cap: {millify(currency.marketCap)}/</p>
+                  <p>Daily Change: {millify(currency.change)}%/</p>
+                </Card>
+                
+              </Link>
+            </Col>
+          );
+        })}
+        
       </Row>
     </>
   );
