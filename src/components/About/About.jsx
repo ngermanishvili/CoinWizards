3bee867f55e8607bcf2ca3ed670c245dff401c75
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-07-23 21:07:33 +0400

    🚀

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,37 +1,9 @@
-import Bubbles from "../AnimatedBubbles/AnimatedBubbles"; // Import the Bubbles component
-import styled from "styled-components";
 import React from 'react'
 
-export const About = () => {
+const About = () => {
   return (
-    <>
-      <Wrapper>
-        <div className="bubblediv">
-          <Bubbles />
-        </div>
-      </Wrapper>
-    </>
-  );
-};
+    <div>About</div>
+  )
+}
 
-
-const Wrapper = styled.div`
-  .bubblediv {
-    margin-top: 50px;
-    width: 100%;
-    height: 800px;
-    position: relative;
-    background: #1a1a1a;
-  }
-//
-  @media screen and (max-width: 800px) {
-    .bubblediv {
-      width: 100%;
-      height: 100vh;
-      bottom: 0;
-      position: absolute;
-    }
-  }
-`;
-
-export default Bubbles; // Export the Bubbles component with a default export
+export default About
