151883ea484c63024bde3bfd4d4bcb8d9fc04cae
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-08-08 22:37:06 +0400

    update crypto api modified querys and export it to another component

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -22,6 +22,7 @@ export const cryptoApi = createApi({
             query: ({ coinId, timeperiod }) => createRequest(`coin/${coinId}/history?timeperiod=${timeperiod}`),
         }),
     })
+    //
 
 });
 
